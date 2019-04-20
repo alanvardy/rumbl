fed70ecabfb6cf7fa8b4c9b88e1b0ab7c61fbe47
Alan Vardy <alanvardy@gmail.com>
2019-04-20 08:50:19 -0600

    wip

--- mix.exs
+++ mix.exs
@@ -44,7 +44,8 @@ defmodule Rumbl.MixProject do
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
       {:comeonin, "~> 4.1"},
-      {:pbkdf2_elixir, "~> 0.12"}
+      {:pbkdf2_elixir, "~> 0.12"},
+      {:ex_doc, "~> 0.20"}
     ]
   end
 
